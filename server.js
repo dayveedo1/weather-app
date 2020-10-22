a8659f3515cb8570a724ef587407c9ec9c780bba
dayveedo1 <dayveedo1@gmail.com>
2020-10-22 16:55:36 +0100

    installed express and path & deployed on Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+// Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+// Serve only the static yes files form the dist directory
+app.use(express.static(__dirname + '/dist/weather-app'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
+});
+// Start the app by listening on the default Heroku
+app.listen(process.env.PORT || 5050);
